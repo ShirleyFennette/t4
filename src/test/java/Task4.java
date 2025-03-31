8661aa65ad3f841349a0f675f2f43d351110fa0c
ShirleyFennette <dkopylov@qatools.ru>
2025-03-31 22:22:35 +0300

    gitignore + buildgradle + fullcode

---
+++ src/test/java/Task4.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+        public class Task4 {
+            {
+                Configuration.pageLoadStrategy = "eager";
+                Configuration.browserSize = "1920x1080";
+                Configuration.baseUrl = "https://github.com";
+
+            }
+            @Test
+            void Task4test() {
+                open("/selenide/selenide");
+                $("#wiki-tab").shouldHave(text("wiki")).click();
+                $("#wiki-tab").click();
+                $$("[href$='SoftAssertions']").first().shouldHave((text("Soft assertions")));
+                $$("[href$='SoftAssertions']").first().click();
+                $$("h4.heading-element").findBy(text("JUnit5")).should(exist);
+                $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class}) " +
+                        "class Tests { " +
+                        "  @Test " +
+                        "  void test() { " +
+                        "    Configuration.assertionMode = SOFT; " +
+                        "    open(\"page.html\"); " +
+                        "    $(\"#first\").should(visible).click(); " +
+                        "    $(\"#second\").should(visible).click(); " +
+                        "  } " +
+                        "}"));
+            }}
